58014cef4cd8290239fb14469652c9a42aa2d187
ashish <ashishmangla1991@gmail.com>
2023-03-21 16:31:39 +0530

    a

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,11 +5,35 @@ import logo from "../../resources/logowhite.svg";
 const Header = () => {
   return (
     <header className="header">
-      <div className="container">
+      <div className="header__container">
         <a href="/" title="Logo">
           <img src={logo} alt="Logo" />
         </a>
+        <div className="header__right">
+          <nav></nav>
+          <button role="switch" className="header__nav-btn">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
       </div>
+
+      <nav>
+        <ul>
+          <li>
+            <a>Team</a>
+          </li>
+          <li>
+            <a>Team</a>
+          </li>
+          <li>
+            <a className="nav__button" href="/">
+              Team
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
